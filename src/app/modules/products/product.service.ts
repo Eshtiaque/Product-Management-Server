789dfa4b636b7559c94b7b1d17b519c95589ad8c
Eshtiaque <est.ahmed111@gmail.com>
2024-05-22 18:14:23 +0600

    Product

--- src/app/modules/products/product.service.ts
+++ src/app/modules/products/product.service.ts
@@ -1,28 +1,29 @@
 import { Product } from './product.interface';
 import { ProductModel } from './product.model';
 
+//create data with product
 const createProductIntoDB = async (product: Product) => {
   const result = await ProductModel.create(product);
   return result;
 };
-
+//gate all product
 const getAllProductsFromDB = async (
   searchTerm: object,
 ): Promise<Product[] | null> => {
   const result = await ProductModel.find(searchTerm);
   return result;
 };
-
+// gate single product
 const getSingleProductFromDB = async (_id: string) => {
   const result = await ProductModel.findOne({ _id });
   return result;
 };
-
+//delete product from database
 const deleteProductFromDB = async (_id: string) => {
   const result = await ProductModel.deleteOne({ _id });
   return result;
 };
-
+//update a single product
 const updateProductFromDB = async (
   _id: string,
   updateData: Partial<Product>,
@@ -35,7 +36,7 @@ const updateProductFromDB = async (
     return result;
   } catch (error) {
     console.error(`Failed to update product with id ${_id}:`, error);
-    throw error; // Propagate the error for higher-level handling
+    throw error; 
   }
 };
 
